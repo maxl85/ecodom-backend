d093213926f25d5dd6a5729d4d5f7df1ad4ef107
maxl85 <maxl85@mail.ru>
2022-08-03 17:33:00 +0300

    Up to 2 lessons

---
+++ backend/base/views/product_views.py
@@ -0,0 +1,21 @@
+import imp
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from ..models import Product
+from ..serializer import ProductSerializer
+
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
